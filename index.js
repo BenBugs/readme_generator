ee3715d4015911ce09374031298ead6e8e96db8e
Ben Cole <benedictcole1@gmail.com>
2020-04-23 13:56:59 +0930

    finished promise for inquirer

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
-const questions = [
+const inquirer = require("inquirer");
 
-];
+function getUserInput() {
 
-function writeToFile(fileName, data) {
+    let questions = [
+        // {
+        //     type: 'input',
+        //     name: 'project_title',
+        //     message: 'Add a Project Title',
+        // },
+        {
+            type: 'input',
+            name: 'github_username',
+            message: 'What is your GitHub username?',
+        }
+
+    ];
+
+    return inquirer.prompt(questions); // inquirer.prompt asks the q's and returns the answers to the promise
+
 }
 
-function init() {
+getUserInput().then(function (response) {
+    console.log(response);
+    getProfile(response.github_username)
+})
 
+
+function getProfile(username) {
+    let queryUrl = `https://api.github.com/users/${username}`;
+
+    console.log(queryUrl);
 }
 
-init();
+
+
+
+
+
+// .then(function (response) {
+
+//     if (response.confirm === response.password) {
+//         console.log("Success!");
+//     }
+//     else {
+//         console.log("You forgot your password already?!");
+//     }
+// });
+
+
+
+// function writeToFile(fileName, data) 
+// {
+
+
+
+// }
+
+// function init() {
+
+// }
+
+// init();
---
+++ psuedoCode.js
@@ -0,0 +1,28 @@
+// STAGE 1
+
+//when i need to create a readme file for a opensource project:
+// 1. i need to create a new folder from the command line
+// 2. or i need to navigate to a newly created local directory 
+// 3. i need to invoke the readme application from the command line useing 'node index.js'
+
+// STAGE 2
+// get user input
+// 1. add a project title
+// 2. add a description
+// 3. add a table of Contents
+// 4. add installation details
+// 5. Usage
+// 6. License
+// 7. Contributing
+// 8. Tests
+// 9. ask for GitHub username
+// 10. ask for email related to GitHub account
+
+
+// i need to include a badge from gihub by;
+// 1. creating an API template literal and injecting user name
+// 2. calling the API
+// 3. storing the response in a variable
+
+// https://img.shields.io/badge/Project-Read%20Me%20Generator-blue
+
